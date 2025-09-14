ecfdc92930d88b5a53671ee0850dace970110f8e
zm <mizuks@hotmail.co.jp>
2025-09-14 18:00:25 +0900

    product-listing追加

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,16 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule,
     AppRoutingModule
   ],
   providers: [
---
+++ src/app/product/product.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: false,
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.scss'
+})
+export class ProductComponent {
+
+}
